04947790d9602c45d9a62a4eaaf810a0818ad480
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2022-10-13 13:23:22 -0500

    removed StrictMode

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+import { SpotifyProvider } from "./context/SpotifyContext";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <SpotifyProvider>
     <App />
-  </React.StrictMode>
-)
+  </SpotifyProvider>
+);
