b6f220f314d95a8bb586e586092e2720e201cc2e
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2022-10-13 13:31:43 -0500

    handles spotify authentication

---
+++ server/src/controllers/login.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import SpotifyWebApi from "spotify-web-api-node";
+
+export const login = (req: Request, res: Response) => {
+  const code = req.body.code;
+
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: "http://localhost:5000",
+    clientId: "013a8341148c440caa3fe56fa4742c7c",
+    clientSecret: "086befc1c9e04ced86d0e0b8dd019ede",
+  });
+
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then((data) => {
+      res.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(400);
+    });
+};
