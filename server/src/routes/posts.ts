50dcfad3b74916f6d837772383bc91cd8d8e79f8
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2022-10-13 13:32:45 -0500

    init commit

--- server/src/routes/posts.ts
+++ server/src/routes/posts.ts
@@ -1,10 +1,20 @@
 import express from "express";
+// import bodyParser from "body-parser";
 
 import { getPosts, createPost } from "../controllers/posts.js";
+// import { login } from "../controllers/login.js";
 
+const app = express();
+// app.use(bodyParser.json());
+
+// gotta test whether you need app + bodyparser here or inside login function
 const router = express.Router();
+// const app2 = express();
 
 router.get("/", getPosts);
 router.post("/", createPost);
+
+// router.post("/login", login);
+// app2.post("/login", login);
 
 export default router;
