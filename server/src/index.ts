dfad760eb7f0f93032c89758c7c407f41ca71fd9
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2022-10-13 13:30:41 -0500

    changed to port 5001 + handling for new routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,30 @@
-import express from "express";
-import bodyParser from "body-parser";
+import express, { Request, Response } from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+
 import postRoutes from "./routes/posts.js";
+import loginRoute from "./routes/login.js";
+
+import { login } from "./controllers/login.js";
+import { refresh } from "./controllers/refresh.js";
+import { getLyrics } from "./controllers/getLyrics.js";
 
 const app = express();
 
-app.use("/posts", postRoutes);
-
-app.use(bodyParser.json()); // {limit: "30mb", extended: true }
-app.use(bodyParser.urlencoded({ extended: true })); // limit: "30mb"
 app.use(cors());
+app.use(express.json()); // {limit: "30mb", extended: true }
+app.use(express.urlencoded({ extended: true })); // limit: "30mb"
+
+// app.use("/posts", postRoutes);
+
+// later on see if you can move these two below into /routes/login.ts
+app.use("/login", login);
+app.use("/refresh", refresh);
+app.use("/user-songs", getLyrics);
 
 const CONNECTION_URL =
   "mongodb+srv://mongaro:LJxNYsUShm5p05gB@cluster0.tj1cbrr.mongodb.net/?retryWrites=true&w=majority";
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 mongoose
   .connect(CONNECTION_URL) // , { useNewUrlParser: true, useUnifiedTopology: true } may not be necessary
@@ -22,8 +32,3 @@ mongoose
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
   )
   .catch((error) => console.log(error.message));
-
-// when you restart today focus on HOW this guy is structuring/connecting front + back end rather than exactly WHAT he is putting in there.
-
-// again big change from before is that client and server side code are completely separate here, and only communicate through a very specific pattern
-// we get to find out what that looks like today! :3 YOU GOT THIS MY FRIEND (ENJOY THE BELL BUT DON'T START THIS WITHOUT PMA FLOWING THROUGH YOU MY NIBBLITS)
