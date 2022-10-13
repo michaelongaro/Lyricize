cccb20dae5e6810eb89472efa4c98df154175591
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2022-10-13 13:26:51 -0500

    spotify auth hook that auto refreshes token

---
+++ client/src/hooks/useAuth.ts
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+
+import axios from "axios";
+
+export default function useAuth(code: string) {
+  const [accessToken, setAccessToken] = useState();
+  const [refreshToken, setRefreshToken] = useState();
+  const [expiresIn, setExpiresIn] = useState();
+
+  useEffect(() => {
+    axios
+      .post("http://localhost:5001/login", {
+        code,
+      })
+      .then((res) => {
+        setAccessToken(res.data.accessToken);
+        setRefreshToken(res.data.refreshToken);
+        setExpiresIn(res.data.expiresIn);
+        window.history.pushState({}, "", "/");
+      })
+      .catch(() => {
+        // workaround for weird ts interaction
+        (window as Window).location = "/";
+      });
+  }, [code]);
+
+  useEffect(() => {
+    if (!refreshToken || !expiresIn) return;
+
+    const interval = setInterval(() => {
+      axios
+        .post("http://localhost:5001/refresh", {
+          refreshToken,
+        })
+        .then((res) => {
+          console.log("received", res.data);
+
+          setAccessToken(res.data.accessToken);
+          setExpiresIn(res.data.expiresIn);
+        })
+        .catch(() => {
+          // workaround for weird ts interaction
+          (window as Window).location = "/";
+        });
+    }, (expiresIn - 60) * 1000);
+
+    return () => clearInterval(interval);
+  }, [refreshToken, expiresIn]);
+
+  return accessToken;
+}
