beca2e62eea0e53d4ab925d8fb9379ac7102e9ab
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2022-10-13 13:31:58 -0500

    keeps spotify authentication valid

---
+++ server/src/controllers/refresh.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import SpotifyWebApi from "spotify-web-api-node";
+
+export const refresh = (req: Request, res: Response) => {
+  const refreshToken = req.body.refreshToken;
+
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: "http://localhost:5000",
+    clientId: "013a8341148c440caa3fe56fa4742c7c",
+    clientSecret: "086befc1c9e04ced86d0e0b8dd019ede",
+    refreshToken,
+  });
+
+  spotifyApi
+    .refreshAccessToken()
+    .then((data) => {
+      res.json({
+        accessToken: data.body.access_token,
+        expiresIn: data.body.expires_in,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(400);
+    });
+};
