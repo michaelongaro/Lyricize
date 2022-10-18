c4adb7d257010b2d7f0df135e62387730b0e0554
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2022-10-18 17:44:09 -0500

    added username to post body

--- client/src/util/sendSongsToBackend.ts
+++ client/src/util/sendSongsToBackend.ts
@@ -1,20 +1,20 @@
+// import { useState } from "react";
 import axios from "axios";
 
-export default function sendSongsToBackend(songs: string[]) {
-  // const [accessToken, setAccessToken] = useState();
-  // const [refreshToken, setRefreshToken] = useState();
-  // const [expiresIn, setExpiresIn] = useState();
+export default function sendSongsToBackend(
+  currentUsername: string,
+  songs: string[]
+) {
+  // const [sortedLyrics, setSortedLyrics] = useState();
 
   axios
     .post("http://localhost:5001/user-songs", {
+      currentUsername,
       songs,
     })
     .then((res) => {
-      // setAccessToken(res.data.accessToken);
-      // setRefreshToken(res.data.refreshToken);
-      // setExpiresIn(res.data.expiresIn);
-      // window.history.pushState({}, "", "/");
-      // setSortedLyrics();
+      // setSortedLyrics(res.data);
+      return res.data;
     })
     .catch(() => {
       // workaround for weird ts interaction
