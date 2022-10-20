139963d4e7bf1a8f037a346691d7c7cf14d1d657
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2022-10-20 02:58:54 -0500

    actual lyric source switching functionality

--- client/src/components/PersonalGlobalToggle/PersonalGlobalToggle.tsx
+++ client/src/components/PersonalGlobalToggle/PersonalGlobalToggle.tsx
@@ -1,4 +1,11 @@
-import React from "react";
+import { useContext } from "react";
+
+import SpotifyContext from "../../context/SpotifyContext";
+
+import RefreshData from "../UserData/RefreshData";
+
+import userIcon from "../../assets/user.svg";
+import worldIcon from "../../assets/world.svg";
 
 import classes from "./PersonalGlobalToggle.module.css";
 import "../../index.css";
@@ -6,10 +13,36 @@ import "../../index.css";
 type Props = {};
 
 function PersonalGlobalToggle({}: Props) {
+  const spotifyCtx = useContext(SpotifyContext);
+
   return (
-    <div className={`${classes.toggleContain} baseFlex`}>
-      <button>My Stats</button>
-      <button>Global Stats</button>
+    <div
+      style={{ marginTop: "1rem", gap: "1.5rem" }}
+      className={"buttonContainer baseVertFlex"}
+    >
+      <div className={classes.logo}>Lyricize</div>
+      <div className={"baseFlex"}>
+        <button
+          onClick={() => spotifyCtx?.setShowUserLyrics(true)}
+          className={`${classes.toggleButton} ${
+            spotifyCtx?.showUserLyrics ? "toggledOn" : ""
+          } baseFlex`}
+        >
+          <img src={userIcon} className={classes.icon} />
+          My Stats
+        </button>
+        <button
+          onClick={() => spotifyCtx?.setShowUserLyrics(false)}
+          className={`${classes.toggleButton} ${
+            !spotifyCtx?.showUserLyrics ? "toggledOn" : ""
+          } baseFlex`}
+        >
+          <img src={worldIcon} className={classes.icon} />
+          Global Stats
+        </button>
+
+        <RefreshData />
+      </div>
     </div>
   );
 }
