a3c9d6bb50a567730d2aa2da17346684e515504d
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2022-10-20 03:07:33 -0500

    also returns global db results in unified obj

--- server/src/controllers/isUserInDatabase.ts
+++ server/src/controllers/isUserInDatabase.ts
@@ -1,15 +1,24 @@
 import { Request, Response } from "express";
 
-import { UserLyrics } from "../models/lyrics.js";
+import { GlobalLyrics, UserLyrics } from "../models/lyrics.js";
 
 export const isUserInDatabase = async (req: Request, res: Response) => {
   const currentUsername = req.body.currentUsername;
 
   UserLyrics.find({ spotifyUsername: currentUsername })
     .exec()
-    .then((response) => {
-      if (response.length > 0) {
-        res.json(response[0]["sortedLyrics"]);
+    .then((userResponse) => {
+      if (userResponse.length > 0) {
+        GlobalLyrics.find({})
+          .exec()
+          .then((globalResponse) => {
+            if (globalResponse.length > 0) {
+              res.json({
+                user: userResponse[0]["sortedLyrics"],
+                global: globalResponse[0]["sortedLyrics"],
+              });
+            }
+          });
       }
     });
 };
