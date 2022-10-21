94a8bc1da46cc1a2f1cb8dd6fe6ae5f8fbb315d8
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2022-10-21 03:36:35 -0500

    fixed build path

--- src/index.ts
+++ src/index.ts
@@ -29,9 +29,9 @@ const CONNECTION_URL = process.env.MONGO_URI!;
 const PORT = process.env.PORT || 5001;
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("client/dist"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
   });
 }
 
