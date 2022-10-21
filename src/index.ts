a7c0aa3c864ca1c54d05395b97607962d954f654
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2022-10-21 03:47:23 -0500

    added dynamic __dirname to be served

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,7 @@ const CONNECTION_URL = process.env.MONGO_URI!;
 const PORT = process.env.PORT || 5001;
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/dist"));
+  app.use(express.static(path.join(__dirname, "client/dist")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
   });
