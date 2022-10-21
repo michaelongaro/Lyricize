315826441606558fb5d967a45cbd969a145209a3
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2022-10-21 02:39:43 -0500

    fixed build output

--- client/src/App.js
+++
@@ -1,7 +0,0 @@
-import Hero from "./components/Hero/Hero";
-import MapTableToggle from "./components/Visualizations/MapTableToggle";
-const code = new URLSearchParams(window.location.search).get("code");
-function App() {
-    return <>{code ? <MapTableToggle code={code}/> : <Hero />}</>;
-}
-export default App;
--- client/src/main.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { SpotifyProvider } from "./context/SpotifyContext";
-import "./index.css";
-ReactDOM.createRoot(document.getElementById("root")).render(<SpotifyProvider>
-    <App />
-  </SpotifyProvider>);
