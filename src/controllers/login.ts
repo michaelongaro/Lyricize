7da262ab5d54c3866b83bdecd31fbc1c323a1a8d
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2022-10-22 12:53:19 -0500

    added trailing / to redirectUri

--- src/controllers/login.ts
+++ src/controllers/login.ts
@@ -5,7 +5,7 @@ export const login = (req: Request, res: Response) => {
   const code = req.body.code;
 
   const spotifyApi = new SpotifyWebApi({
-    redirectUri: "https://fierce-mesa-30544.herokuapp.com",
+    redirectUri: "https://fierce-mesa-30544.herokuapp.com/",
     clientId: "013a8341148c440caa3fe56fa4742c7c",
     clientSecret: "086befc1c9e04ced86d0e0b8dd019ede",
   });
