c1286be7e64632cc0d1baead798d861311eb67ad
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2022-10-27 13:45:17 -0500

    increased payload limit

--- src/index.ts
+++ src/index.ts
@@ -17,8 +17,8 @@ console.log("reached index.js");
 const app = express();
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "500mb" }));
+app.use(express.urlencoded({ extended: true, limit: "500mb" }));
 
 // later on see if you can move these two below into /routes/login.ts
 app.use("/login", login);
