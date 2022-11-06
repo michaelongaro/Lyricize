706c859fb5025749fe6e044d7cbd10465cab351f
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2022-11-06 12:11:18 -0600

    init commit

---
+++ client/src/components/LoadingModal/ProgressBar.tsx
@@ -0,0 +1,67 @@
+import { useContext } from "react";
+import SpotifyContext from "../../context/SpotifyContext";
+
+import { Line } from "rc-progress";
+
+import "../../index.css";
+
+type Props = {
+  estimatedSecondsRemainingStr: string;
+  progressBarTimeElapsed: number;
+  totalTime: number;
+};
+
+function ProgressBar({
+  estimatedSecondsRemainingStr,
+  progressBarTimeElapsed,
+  totalTime,
+}: Props) {
+  const spotifyCtx = useContext(SpotifyContext);
+
+  const estimatedProgressBarTimeElapsed =
+    totalTime - parseInt(estimatedSecondsRemainingStr);
+
+  const estimatedTimeElapsed = totalTime - progressBarTimeElapsed;
+
+  const percentOfSongsFetched = Math.floor(
+    (estimatedTimeElapsed / totalTime) * 100
+  );
+
+  let currentSong: string = "";
+
+  if (spotifyCtx?.userSongList) {
+    const songIdx = Math.floor(
+      (estimatedProgressBarTimeElapsed / totalTime) *
+        spotifyCtx.userSongList.length
+    );
+    currentSong = spotifyCtx.userSongList[songIdx];
+    currentSong = currentSong.replace(",", " - ");
+  }
+
+  return (
+    <div style={{ gap: "1rem" }} className={"baseVertFlex"}>
+      <div style={{ width: "15rem" }}>
+        <Line
+          percent={percentOfSongsFetched}
+          strokeWidth={10}
+          strokeColor="#1DB954"
+          trailWidth={10}
+        />
+      </div>
+      <div
+        style={{
+          gap: ".5rem",
+          width: "25rem",
+          whiteSpace: "nowrap",
+          overflow: "hidden",
+          textOverflow: "ellipsis",
+        }}
+        className={"baseVertFlex"}
+      >
+        <div>{currentSong}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ProgressBar;
