2ded54585662c78253cc5a32dcae046a1bb4bf77
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2022-11-16 15:21:40 -0600

    added/tweaked necessary state for updated hooks

--- client/src/context/SpotifyContext.tsx
+++ client/src/context/SpotifyContext.tsx
@@ -4,34 +4,45 @@ import SpotifyWebApi from "spotify-web-api-node";
 
 import { useFetchUserLyrics } from "../hooks/useFetchUserLyrics";
 import useIncrementallyFetchSongLyrics from "../hooks/useIncrementallyFetchSongLyrics";
-import useSendSongsToBackend from "../hooks/useSendSongsToBackend";
-
 interface ISpotifyContext {
   accessToken: string | null;
-  setAccessToken: Function; // these should be proper react setState types
+  setAccessToken: React.Dispatch<React.SetStateAction<string | null>>;
 
   userSongList: string[] | null;
   totalLikedSongs: number | null;
 
   incrementalIndex: number;
+  setIncrementalIndex: React.Dispatch<React.SetStateAction<number>>;
+  incrementalUserLyrics: [string, number][] | null;
+  setIncrementalUserLyrics: React.Dispatch<
+    React.SetStateAction<[string, number][]>
+  >;
 
   // dont know if you need to expose all of this
   userLyrics: [string, number][] | null;
-  setUserLyrics: Function;
+  setUserLyrics: React.Dispatch<
+    React.SetStateAction<[string, number][] | null>
+  >;
   globalLyrics: [string, number][] | null;
-  setGlobalLyrics: Function;
+  setGlobalLyrics: React.Dispatch<
+    React.SetStateAction<[string, number][] | null>
+  >;
+  userID: string | null;
 
-  refreshLyrics: boolean; // needed later for the loading spinner?
-  setRefreshLyrics: Function;
+  refreshLyrics: boolean;
+  setRefreshLyrics: React.Dispatch<React.SetStateAction<boolean>>;
 
   currentlySelectedLyrics: [string, number][] | null;
-  setCurrentlySelectedLyrics: Function;
+  setCurrentlySelectedLyrics: React.Dispatch<
+    React.SetStateAction<[string, number][] | null>
+  >;
 
   showUserLyrics: boolean;
   setShowUserLyrics: React.Dispatch<React.SetStateAction<boolean>>;
 
   currentPFP: string | null;
   currentUsername: string | null;
+  setCurrentUsername: React.Dispatch<React.SetStateAction<string | null>>;
 }
 
 const SpotifyContext = createContext<ISpotifyContext | null>(null);
@@ -41,7 +52,7 @@ const spotifyApi = new SpotifyWebApi({
 });
 
 export function SpotifyProvider(props: any) {
-  const [accessToken, setAccessToken] = useState(null);
+  const [accessToken, setAccessToken] = useState<string | null>(null);
 
   const [userSongList, setUserSongList] = useState<string[] | null>(null);
   const [totalLikedSongs, setTotalLikedSongs] = useState<number | null>(null);
@@ -50,13 +61,17 @@ export function SpotifyProvider(props: any) {
   const [currentUsername, setCurrentUsername] = useState<string | null>(null);
 
   const [incrementalUserLyrics, setIncrementalUserLyrics] = useState<
-    [string, number][] | null
-  >(null);
+    [string, number][]
+  >([]);
   const [incrementalIndex, setIncrementalIndex] = useState<number>(0);
+  const [currentlyFetchingSongs, setCurrentlyFetchingSongs] =
+    useState<boolean>(false);
+
   const [userLyrics, setUserLyrics] = useState<[string, number][] | null>(null);
   const [globalLyrics, setGlobalLyrics] = useState<[string, number][] | null>(
     null
   );
+  const [userID, setUserID] = useState<string | null>(null);
 
   const [currentlySelectedLyrics, setCurrentlySelectedLyrics] = useState<
     [string, number][] | null
@@ -77,9 +92,6 @@ export function SpotifyProvider(props: any) {
             const imageResults = res.body.images;
             if (imageResults && imageResults.length > 0) {
               setCurrentPFP(imageResults[0].url);
-            } else {
-              // make it falsey in some way
-              setCurrentPFP("User has no PFP");
             }
             setCurrentUsername(res.body.id);
           })
@@ -99,19 +111,27 @@ export function SpotifyProvider(props: any) {
   }, [userLyrics, globalLyrics, showUserLyrics]);
 
   // is this a proper situation/setup for hooks?
-  useIncrementallyFetchSongLyrics(
+  useFetchUserLyrics(
     currentUsername,
     setUserLyrics,
     setGlobalLyrics,
+    setUserID,
     setRefreshLyrics
   );
 
-  useSendSongsToBackend(
+  useIncrementallyFetchSongLyrics(
     currentUsername,
     userSongList,
     totalLikedSongs,
+    incrementalIndex,
+    setIncrementalIndex,
+    incrementalUserLyrics,
+    setIncrementalUserLyrics,
+    currentlyFetchingSongs,
+    setCurrentlyFetchingSongs,
     setUserLyrics,
     setGlobalLyrics,
+    setUserID,
     setRefreshLyrics
   );
 
@@ -120,8 +140,6 @@ export function SpotifyProvider(props: any) {
 
   useEffect(() => {
     if (refreshLyrics) {
-      console.log("refreshing lyrics");
-
       spotifyApi.getMySavedTracks({ limit: 1 }).then((res) => {
         setTotalLikedSongs(res.body.total);
       });
@@ -176,18 +194,20 @@ export function SpotifyProvider(props: any) {
     }
   };
 
-  const context = {
+  const context: ISpotifyContext = {
     accessToken,
     setAccessToken,
     userSongList,
     totalLikedSongs,
     incrementalIndex,
+    setIncrementalIndex,
     incrementalUserLyrics,
     setIncrementalUserLyrics,
     userLyrics,
     setUserLyrics,
     globalLyrics,
     setGlobalLyrics,
+    userID,
     refreshLyrics,
     setRefreshLyrics,
     currentlySelectedLyrics,
@@ -196,6 +216,7 @@ export function SpotifyProvider(props: any) {
     setShowUserLyrics,
     currentPFP,
     currentUsername,
+    setCurrentUsername,
   };
 
   return (
