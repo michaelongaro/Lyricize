18a3d817411aa8e6ca9725e6d0de6e77dd978009
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2022-11-16 15:31:03 -0600

    creates/updates user and calls function to update global data

--- src/controllers/updateDatabase.ts
+++ src/controllers/updateDatabase.ts
@@ -1,21 +1,47 @@
 import { Request, Response } from "express";
+import ShortUniqueId from "short-unique-id";
 
 import { UserLyrics } from "../models/lyrics.js";
 
-export const updateDatabase = async (req: Request, res: Response) => {
-  let finalGlobalResult: [string, number][] = [];
+import recalculateAndUpdateGlobalCollection from "../util/recalculateAndUpdateGlobalCollection.js";
 
-  UserLyrics.findOneAndUpdate(
-    { spotifyUsername: currentUsername },
-    { sortedLyrics: req.body.finalUserResult },
-    { upsert: true },
-    function (err, doc) {
-      if (err) {
-        console.log("error was:", err);
-      } else {
-        recalculateAndUpdateGlobalCollection(finalGlobalResult);
-        res.json({ user: req.finalUserResult, global: finalGlobalResult });
+export const updateDatabase = async (req: Request, res: Response) => {
+  const currentUsername = req.body.currentUsername;
+  const finalUserResult = req.body.finalUserResult;
+
+  // checking to see if user exists in database
+  UserLyrics.find({ spotifyUsername: currentUsername })
+    .exec()
+    .then((userResponse) => {
+      if (userResponse.length > 0) {
+        userResponse[0].sortedLyrics = finalUserResult;
+        userResponse[0].save().then(() => {
+          recalculateAndUpdateGlobalCollection(
+            finalUserResult,
+            userResponse[0].shortUUID,
+            res
+          );
+        });
       }
-    }
-  );
+      // adding new user doc to collection
+      else {
+        const uid = new ShortUniqueId({ length: 10 });
+        const shortUUID = uid();
+
+        UserLyrics.create(
+          {
+            spotifyUsername: currentUsername,
+            shortUUID: shortUUID,
+            sortedLyrics: finalUserResult,
+          },
+          () => {
+            recalculateAndUpdateGlobalCollection(
+              finalUserResult,
+              shortUUID,
+              res
+            );
+          }
+        );
+      }
+    });
 };
