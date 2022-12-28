c1b1ecf1f2cb3916caf528e17ed6ca2794dcc92f
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2022-12-27 22:32:21 -0600

    screen fill animation on click of login button

--- client/src/components/Authentication/Login.tsx
+++ client/src/components/Authentication/Login.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 
 import spotifyIcon from "../../assets/spotify.png";
 
@@ -16,15 +16,36 @@ if (redirectUri !== "http://localhost:5000") {
 const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=013a8341148c440caa3fe56fa4742c7c&response_type=code&redirect_uri=${redirectUri}&scope=user-library-read%20user-read-email%20user-read-private`;
 
 function Login({}: Props) {
+  const [animationStarted, setAnimationStarted] = useState(false);
+
   return (
-    <a
-      style={{ gap: ".75rem" }}
+    <button
+      style={{
+        gap: ".75rem",
+        zIndex: animationStarted ? 1000 : 1,
+        transform: `scale(${animationStarted ? 200 : 1}`,
+        pointerEvents: animationStarted ? "none" : "auto",
+        transition: animationStarted ? "all 2000ms" : "all 150ms",
+      }}
       className={`${classes.loginButton} baseFlex`}
-      href={AUTH_URL}
+      onClick={() => {
+        setTimeout(() => (window.location.href = AUTH_URL), 350);
+        setAnimationStarted(true);
+      }}
     >
-      <div>Login</div>
-      <img src={spotifyIcon} alt={"Spotify Logo"}></img>
-    </a>
+      <div
+        style={{
+          color: animationStarted ? "#1DB954" : "white",
+        }}
+      >
+        Login
+      </div>
+      <img
+        style={{ opacity: animationStarted ? 0 : 1 }}
+        src={spotifyIcon}
+        alt={"Spotify Logo"}
+      ></img>
+    </button>
   );
 }
 
